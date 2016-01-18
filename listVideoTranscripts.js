1749395fb4026aeca194933902eee76c53ffd235
vagrant <tkingless>
2016-01-18 15:41:10 +0000

    can query by find()

--- listVideoTranscripts.js
+++ listVideoTranscripts.js
@@ -1,19 +1,10 @@
 db = connect("localhost:27017/edxapp");
 
-var array = db.modulestore.find({"_id.course": "LTTC002", "_id.category": "video"}).pretty();
-var text = JSON.stringify(array, null, '\t');
+var cursor = db.modulestore.find({"_id.course": "LTTC002", "_id.category": "video"});
 
-//print(text);
-print(array);
-
-//examples for queries
-/*
-db = connect("localhost:27017/test");
-var cursor = db.unicorns.find({gender : "m"});
-var total = 0;
 while(cursor.hasNext()) {
   var obj = cursor.next();
-  total += (obj.hits || 0);
+  var id = obj._id;
+  //print(tojson(id));
+  print(tojson(obj));
 }
-print(total);
-*/
